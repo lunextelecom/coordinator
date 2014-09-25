3e1085017517a6cfbb33c675a94e86e4eaff66b3
tungtruong@lunextelecom.com <tungtruong@lunextelecom.com>
2014-09-25 10:24:55 +0700

    fix djangoenv

--- lunex/coordinator/queue/djangoenv.py
+++ lunex/coordinator/queue/djangoenv.py
@@ -8,11 +8,15 @@ import sys, os;
 #Setting up environment
 basedir = os.path.dirname(os.path.abspath(os.path.realpath(__file__)));
 real_path = os.path.normpath(os.path.join(basedir,'../../..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'lunex.topup.admin.settings'
-sys.path.append('D:/WORKSPACE/vnumbersvc/lunex/vnumbersvc')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'lunex.coordinator.admin.settings'
+# sys.path.append('D:/WORKSPACE/vnumbersvc/lunex/vnumbersvc')
 
 #server
-# sys.path.append('D:/WORKSPACE/vnumbersvc/lunex/vnumbersvc')
+sys.path.append('/usr/local/lib/lunex-apps/common/src')
+sys.path.append('/usr/local/lib/lunex-apps/coordinator/src')
+sys.path.append('/usr/local/lib/lunex-apps/coordinator/libs/bottle_extra')
+sys.path.append('/usr/local/lib/lunex-apps/coordinator/libs/cache_utils')
+sys.path.append('/usr/local/lib/lunex-apps/coordinator/libs/utilities')
 
 sys.path.append(os.path.abspath(real_path))
 
